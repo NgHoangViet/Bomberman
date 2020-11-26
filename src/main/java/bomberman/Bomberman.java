50beff1520eafe44d1d87b8cf0f43b913fb9d974
NgHoangViet <72347348+NgHoangViet@users.noreply.github.com>
2020-11-26 22:48:04 +0700

    cm

--- src/main/java/bomberman/Bomberman.java
+++ src/main/java/bomberman/Bomberman.java
@@ -6,6 +6,6 @@ import main.java.bomberman.gui.Frame;
 public class Bomberman {
 	
 	public static void main(String[] args) throws BombermanException {
-		Frame mainwindow = new Frame();
+		new Frame();
 	}
 }
